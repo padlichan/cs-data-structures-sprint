585c9049df6fb3978dc3ae9b758b598e6c883b05
Lili <lili.berenyi@gmail.com>
2024-12-03 10:50:32 +0000

    Add solution to task1

--- cs-data-structures-sprint/Exercises/Exercise1.cs
+++ cs-data-structures-sprint/Exercises/Exercise1.cs
@@ -8,9 +8,24 @@ namespace cs_data_structures_sprint.Exercises
 {
     internal class Exercise1
     {       
-        public static void PrintExesAndOhs()
+        public static void PrintExesAndOhs(int n)
         {
-            // write your code here
+            char[,] TwoDArray = new char[n, n];
+            
+            
+            char letter = 'x';
+
+            for (int i = 0; i < TwoDArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < TwoDArray.GetLength(1); j++)
+                {
+                    TwoDArray[i,j] = letter;
+                    if (letter == 'x') letter = 'o';
+                    else letter = 'x';
+                    Console.Write(TwoDArray[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- cs-data-structures-sprint/Program.cs
+++ cs-data-structures-sprint/Program.cs
@@ -1,1 +1,4 @@
 ﻿// Console.WriteLine("Your code can go here, if you like");
+using cs_data_structures_sprint.Exercises;
+
+Exercise1.PrintExesAndOhs(5);
